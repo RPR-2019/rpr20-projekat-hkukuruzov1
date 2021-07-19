d68fd79bc9e0962a82b18da6416bb8f5e0f5ee90
hkukuruzov1 <hkukuruzov1@etf.unsa.ba>
2021-07-19 02:24:26 +0200

    Kreiran izgled novog prozora, koji logovani radnik vidi.

--- src/ba/unsa/etf/rpr/Controller.java
+++ src/ba/unsa/etf/rpr/Controller.java
@@ -2,11 +2,18 @@ package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Controller {
@@ -21,15 +28,35 @@ public class Controller {
     public void actionExit(ActionEvent actionEvent) {
         System.exit(0);
     }
-    public void actionLogin(ActionEvent actionEvent) throws SQLException {
+    public void actionLogin(ActionEvent actionEvent) throws SQLException, IOException {
         if(dao.daLiPostojiKorisnik(lbl.getText(),pass.getText()))
         {
             Korisnik k= dao.dajKorisnika(lbl.getText(),pass.getText());
             if(dao.daLiJeAdmin(k.getUsername(),k.getPassword())){
                //otvori admin prozor
+                Stage stage = (Stage) login.getScene().getWindow();
+                // do what you have to do
+                stage.close();
+                Stage primaryStage=new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
+                primaryStage.setTitle("Clockify");
+                primaryStage.setScene(new Scene(root, 400, 350));
+                primaryStage.setMinHeight(275);
+                primaryStage.setMinWidth(300);
+                primaryStage.show();
             }
             else{
                 //otvori radnik prozor
+                Stage stage = (Stage) login.getScene().getWindow();
+                // do what you have to do
+                stage.close();
+                Stage primaryStage=new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/radnik.fxml"));
+                primaryStage.setTitle("Clockify");
+                primaryStage.setScene(new Scene(root, 400, 350));
+                primaryStage.setMinHeight(275);
+                primaryStage.setMinWidth(300);
+                primaryStage.show();
             }
         }
         else{
