c82bdbef118fdb447a1427fad8874c585576e565
hkukuruzov1 <hkukuruzov1@etf.unsa.ba>
2021-07-20 04:03:41 +0200

    Kreirana obavještenja kada se radnik priavljuje/odjavljuje.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,3 +11,23 @@ Q
 !src/ba/unsa/etf/rpr/Korisnik.java,3\a\3a70354acd51a67dbf40b27e3a518faab4d92b26
 T
 $src/ba/unsa/etf/rpr/KorisnikDao.java,f\0\f05003f1ef02d9e1fe55ddc437e7665e4e49a44c
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+a
+1.idea/libraries/org_xerial_sqlite_jdbc_3_21_0.xml,5\4\5425a9e8197bbc593a3ad60641080ce9498caabd
+C
+res/fxml/admin.fxml,0\e\0e0c73d135306af15be43518364630c2696cf328
+S
+#src/ba/unsa/etf/rpr/mjestoRada.java,b\2\b2088f64dcda6191f65123cce59c7af2a2280959
+B
+res/css/radnik.css,1\e\1ea7632cd2c5583ac8be92141fffe0926b6a028d
+X
+(src/ba/unsa/etf/rpr/AdminController.java,3\1\31b92c906370d9c6d0ac1d8b33d46c2c90b41d7e
+O
+src/ba/unsa/etf/rpr/Radnik.java,6\f\6f6d9827e7ca2d107b173b68b5d1bc372fdde5f3
+=
+korisnici.sql,7\f\7fa0fc1591e48d6d8e10bb35625a819184e21da4
+Y
+)src/ba/unsa/etf/rpr/RadnikController.java,4\8\48a6334030e0309b3498993dde24087aa5a1876f
+<
+korisnici.db,7\e\7e48f36f45e9b9c3b7aab5afa045a76bf3c6b6d2
--- src/ba/unsa/etf/rpr/RadnikController.java
+++ src/ba/unsa/etf/rpr/RadnikController.java
@@ -1,4 +1,56 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+import java.time.LocalTime;
+
 public class RadnikController {
+    public Button ulaz;
+    public Button izlaz;
+    public RadioButton rw;
+    public RadioButton ow;
+    public boolean aktiv;
+    @FXML
+    public void initialize(){
+
+    }
+
+    public void start(ActionEvent actionEvent) {
+        LocalTime s=LocalTime.now();
+        if(s.getHour()<9) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Obavijest");
+            alert.setHeaderText("Obavijest o satnici");
+            alert.setContentText("Počeli ste raditi prijevremeno, satnica će vam se uračuniti!");
+
+            alert.showAndWait();
+        }
+        aktiv=true;
+    }
+
+    public void kraj(ActionEvent actionEvent) {
+        if(aktiv) {
+            LocalTime s=LocalTime.now();
+            if(s.getHour()<16){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Upozorenje");
+                alert.setHeaderText("Upozorenje o satnici");
+                alert.setContentText("Odjavili ste se prije kraja radnog vremena, satnica će vam biti zabilježena!");
+
+                alert.showAndWait();
+            }
+                else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Obavijest");
+                alert.setHeaderText("Obavijest o satnici");
+                alert.setContentText("Radite prekovremeno, satnica će vam se uračuniti!");
+
+                alert.showAndWait();
+            }
+        }
+    }
 }
